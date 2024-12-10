bdff404b9a3a6cd316469213d9222161f08ab4c2
cfsylvan <136009419+cfsylvan@users.noreply.github.com>
2024-12-10 11:47:21 -0500

    Updated to catch duplicate favorites

--- add_favorite.php
+++ add_favorite.php
@@ -21,16 +21,27 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Use a prepare so movies with ' or " can still be inserted into the sql.
-    $s = $conn->prepare("INSERT INTO movies (username, title, year, plot, poster_url, imdbID) 
-    VALUES (?, ?, ?, ?, ?, ?)");
+    //check if movie is a duplicate favorite
+    $check_duplicate ='SELECT * FROM movies WHERE imdbID = ' . $imdbID;
+    $result = $conn->query($check_duplicate);
+    //get result
+    $row = $result->fetch_assoc();
+    $duplicate = ($row != null);
 
-    $s->bind_param("ssisss", $username, $title, $year, $plot, $poster_url, $imdbID);
-    $s->execute();
-    $s->close();
-    $conn->close();
-
-    echo "<h1>Movie successfully added to your Favorites!</h1>";
+    if ($duplicate){
+        echo "<h1>This movie is already added to your Favorites. </h1>";
+    } else {
+        // Use a prepare so movies with ' or " can still be inserted into the sql.
+        $s = $conn->prepare("INSERT INTO movies (username, title, year, plot, poster_url, imdbID) 
+        VALUES (?, ?, ?, ?, ?, ?)");
+    
+        $s->bind_param("ssisss", $username, $title, $year, $plot, $poster_url, $imdbID);
+        $s->execute();
+        $s->close();
+        $conn->close();
+    
+        echo "<h1>Movie successfully added to your Favorites!</h1>";
+    }
     echo "<button id=viewFavorites>View Favorites</button>";
     echo "<button id=searchAgain>Search for Another Movie</button>";
         
